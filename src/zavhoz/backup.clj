2963e15f156ea47b5a878d3ed2bbd6fba6d5abcd
Oleg Veschin <oveshchin@phoenixit.ru>
2024-08-19 13:58:38 +0700

    Добавлена функция бэкапа файлов в zip
    Co-authored-by: oveshchin <oveshchin@phoenixit.ru>

--- src/zavhoz/backup.clj
+++ src/zavhoz/backup.clj
@@ -37,6 +37,11 @@
                  (mapv #(zipmap [:ext :count] %))
                  (p/table! (format "Files stat in '%s'" (-> path io/file .getAbsolutePath))))
         (p/wrap! (format "Dir '%s' doesn't exists" path))))))
+
+(defn- simple-date []
+  (-> "yyyy-MM-dd"
+      (java.text.SimpleDateFormat.)
+      (.format (java.util.Date.))))
 ;; TODO:
 ;; я могу сохранить только путь без начального /
 ;; значит при восстановлении бэкапа надо будет его обязательно добавлять
@@ -108,6 +113,20 @@
             (swap! counter inc))
           (format "Done refresh %s/%s" @counter (count files)))))))
 
+(defn rm! [& [counter]]
+  (fn rm*
+    [path]
+    (when (.isDirectory (io/file path))
+      (->> path (io/file) (.listFiles) (run! rm*)))
+    (when (-> path (io/file) (.isDirectory) (not))
+      (when counter (swap! counter inc))
+      (->> path
+           (io/file)
+           (.getName)
+           (format "- %s")
+           (p/progress!)))
+    (io/delete-file path)))
+
 (defn forget-files! [{[path] :_arguments}]
   (let [path*       (str backup-dir "/" path)
         files-count (some->> path*
@@ -138,12 +157,81 @@
         (when files-count
           (format "Forgotten %s/%s" @counter files-count))))))
 
+;; TODO:
+;; подумать как добавить сюда SQL базу
+(defn backup-zip! [{all? :all path :path}]
+  (let [psize      (fn psize* [f]
+                     (if (.isDirectory (io/file f))
+                       (apply + (pmap psize* (.listFiles (io/file f))))
+                       (.length (io/file f))))
+        zip-path   (str "/tmp/bkp-"(simple-date))
+        path       (if all? backup-dir path)
+        something? (or (or path all?)
+                       (some-> path (io/file) (.exists))
+                       (some-> path (io/file) (file-seq) (rest) (not-empty) (boolean)))]
+    (p/print!
+      (when something?
+        (let [meta (mapv (fn [{path :path upd :updated_at}]
+                           {:path path :updated_at (str upd)})
+                         (if all?
+                           (t2/select [:file :path :updated_at])
+                           (t2/select :file :path path)))]
+          (zip/zip-folder! path zip-path meta)
+          (format "Done backup to '%s' with size %.2f MB"
+                  zip-path
+                  (as-> zip-path p
+                    (psize p)
+                    (/ p 1024 1024)
+                    (float p))))))))
+
+(defn restore-zip! [{remem? :remem [path] :_arguments}]
+  (when (some-> path io/file .exists)
+    (let [tmp-folder "/tmp/.zavhoz_unzip"
+          counter (atom 0)
+          cp! (cp! counter)]
+      (try
+        (let [meta (some->> (str tmp-folder "/zavhoz_meta.edn") slurp read-string)]
+          (zip/unzip! path tmp-folder)
+          (p/table! "Will be restored" meta)
+          (p/with-agree!
+            (doseq [{path* :path} meta]
+              (println ["from" (str tmp-folder path*) "to" path*])
+              ;; TODO:
+              ;; сейчас файлы не копируются, потому что сломаны пути
+              ;; у точки назначения не верный путь
+              ;; [from /tmp/.zavhoz_unzip/home/veschin/.ssh to /home/veschin/.ssh]
+              (cp! (str tmp-folder path*) (-> path*
+                                              (io/file)
+                                              (file-seq)
+                                              (mapv #(-> % .getAbsolutePath
+                                                         (str/replace)))))
+              (when remem?
+                (with-redefs [p/supress? true
+                              p/agree? true]
+                  (mem-files! {:_arguments [path*]}))))
+            (format "Done %s/%s files" @counter
+                    (->> tmp-folder io/file file-seq
+                         (remove #(.isDirectory %))
+                         (rest)
+                         (count)))))
+        (finally (rm! tmp-folder))))))
+
 (comment
+  (.exists (io/file nil))
   (do (with-redefs [read-line (constantly "y")]
         (mem-files! {:_arguments ["/home/veschin/.config/bspwm" nil]}))
       (forget-files! {:_arguments ["/home/veschin/.config/bspwm" nil]}))
   (mem-refresh! {:_arguments []})
   (files-info {:_arguments ["/home/veschin/.config/bspwm" nil]})
+  (with-redefs [p/agree? true]
+    (restore-zip! {:_arguments ["/tmp/bkp-2024-08-19"] :remem true}))
+
+  (zip/zip-folder! backup-dir "/tmp/b.zip" {:a 10})
+  (zip/unzip! "/tmp/bkp-2024-08-19" "/tmp/test")
+
+  (-> "/tmp/test/zavhoz_meta.edn" slurp read-string)
+
+  (str/replace "/tmp/.zavhoz_unzip/home/veschin/.ssh" #"/tmp/.zavhoz_unzip" "")
 
   (t2/select :file)
 
@@ -180,4 +268,22 @@
     :opts        [{:as     "Path to forget"
                    :option "path"
                    :type   :string}]
-    :runs        forget-files!}])
+    :runs        forget-files!}
+   {:command     "bkp-zip"
+    :description "Zip selected or all backups"
+    :opts        [{:as     "Path to forget"
+                   :option "path"
+                   :type   :flag}
+                  {:as     "All?"
+                   :option "all"
+                   :type   :flag}]
+    :runs        backup-zip!}
+   {:command     "restore-zip"
+    :description "Resotore all files to File System and update ZavhozDB from zip"
+    :opts        [{:as     "Path to zip"
+                   :option "path"
+                   :type   :string}
+                  {:as     "Remember restored files?"
+                   :option "remem"
+                   :type   :flag}]
+    :runs        restore-zip!}])
